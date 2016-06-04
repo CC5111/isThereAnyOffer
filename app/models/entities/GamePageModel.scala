61a63ebadbc3410e52bc4f02b092670e31aa9a23
emoreno-94 <edward.moreno@ing.uchile.cl>
2016-06-04 14:03:02 -0400

    Entities creados

---
+++ app/models/entities/GamePageModel.scala
@@ -0,0 +1,52 @@
+package models.entities
+
+case class User(id: Long,
+                username: String,
+                name: String,
+                email: String,
+                birthdate: java.sql.Timestamp,
+                password: String,
+                photo: String) extends BaseEntity
+
+case class WatchListItem(id: Long,
+                         idUser: Long,
+                         idGame: Long,
+                         creationDate: java.sql.Timestamp,
+                         threshold: Int) extends BaseEntity
+
+case class Offer(id: Long,
+                 link: String,
+                 idGame: Long,
+                 fromDate: java.sql.Timestamp,
+                 untilDate: java.sql.Timestamp,
+                 normalPrice: Double,
+                 offerPrice: Double) extends BaseEntity
+
+case class Game(id: Long,
+                name: String,
+                cover: String,
+                publisher: String,
+                rating: String,
+                releaseDate: java.sql.Timestamp,
+                typeGame: String) extends BaseEntity
+
+case class GameCategory(id: Long,
+                        idGame: Long,
+                        idCategory: Long) extends BaseEntity
+
+case class Category(id: Long,
+                    name: String) extends BaseEntity
+
+case class GameGenre(id: Long,
+                     idGame: Long,
+                     idGenre: Long) extends BaseEntity
+
+case class Genre(id: Long,
+                 name: String) extends BaseEntity
+
+case class GamePlatform(id: Long,
+                        idGame: Long,
+                        idPlatform: Long) extends BaseEntity
+
+case class Platform(id: Long,
+                    name: String) extends BaseEntity
