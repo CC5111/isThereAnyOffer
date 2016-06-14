83a23a2291d38557896a87495c3037829190b31a
Felipe Rubilar <felipe.rubilar@ug.uchile.cl>
2016-06-14 14:15:19 -0400

    Actores GOG y PsStore

--- app/actors/GogActor.scala
+++ app/actors/GogActor.scala
@@ -1,28 +1,100 @@
 package actors
 
 
+import java.sql.Timestamp
+import java.util.Date
+
 import akka.actor.{Actor, ActorRef, Props}
-import models.daos.GameDAO
+import models.daos.{GameDAO, OfferDAO}
+import models.entities.Offer
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object GogActor {
-    case class Query(game: String)
+    case class Query(game: Long)
     case class Update()
-    def props(out: ActorRef, gameDAO: GameDAO) =
-        Props(classOf[SteamActor], out, gameDAO)
+    def props(gameDAO: GameDAO, offerDAO: OfferDAO) =
+        Props(classOf[GogActor], gameDAO, offerDAO)
 }
 
-class GogActor extends Actor{
+class GogActor(gameDAO: GameDAO, offerDAO: OfferDAO) extends Actor{
     import context._
     import actors.GogActor._
 
-    // https://www.gog.com/games/ajax/filtered?mediaType=game&search=<query>
+    // https://www.gog.com/games/ajax/filtered?mediaType=game&price=discounted&page=1
+    /*
+        products = [
+            {
+                title = "Witcher 3"
+                buyable = true
+                image = "url"
+                id = 1209819203
+                url = "/game/the_witcher_3_wild_hunt"
+                price = {
+                    amount = "12.49"
+                    baseAmount = "24.99"
+                    finalAmount = "12.49"
+                    isDiscounted = True
+                    discountPercentage = 50
+                    discountDifference = "12.50"
+                    ...
+                }
+                isDiscounted = True
+                salesVisibility = {
+                    isActive = True
+                    fromObject = {
+                        date = "2015-05-19 02:00:00"
+                        ...
+                    }
+                    toObject = {
+                        date = "2020-12-31 02:00:00"
+                        ...
+                    }
+                }
+                category = "Role-Playing"
+                isGame = True
+                ...
+            }, ...
+        ]
+        totalGamesFound = n
+
+     */
+
+    case class OfferData(id: Long, url: String, base_price: Double, discounted_price: Double, start_date: Long, end_date: Long)
+    implicit val offerRead: Reads[OfferData] = (
+        (__ \ "id").read[Long] and
+        (__ \ "url").read[String] and
+        (__ \ "price" \ "baseAmount").read[Double] and
+        (__ \ "price" \ "finalAmount").read[Double] and
+        (__ \ "salesVisibility" \ "from").read[Long] and
+        (__ \ "salesVisibility" \ "to").read[Long])(OfferData.apply _)
 
     def receive = {
         case Query(game) => {
-            // Obtener precios para $game
+            ???
         }
         case Update() => {
-            // Actualizar precios de la DB
+            val response: JsValue = ???
+            val game_ids: Map[String, Long] = ???
+
+            val new_offers = (response \ "products").as[List[OfferData]]
+            val valid_offers =
+                for {
+                    offer <- new_offers
+                    if game_ids contains offer.id.toString
+                } yield Offer(0,
+                    "www.gog.com" + offer.url,
+                    game_ids.get(offer.id.toString) match {
+                        case Some(g) => g
+                        case None => 0
+                    },
+                    5,  // PC
+                    "GOG",
+                    new Timestamp(offer.start_date*1000),
+                    new Timestamp(offer.end_date*1000),
+                    offer.base_price,
+                    offer.discounted_price)
+            offerDAO.insert(valid_offers)
         }
     }
 
--- app/actors/PsActor.scala
+++ app/actors/PsActor.scala
@@ -1,28 +1,105 @@
 package actors
 
+import java.sql.Timestamp
+import java.util.Date
+
 import akka.actor.{Actor, ActorRef, Props}
-import models.daos.GameDAO
+import models.daos.{GameDAO, OfferDAO}
+import models.entities.Offer
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object PsActor {
-    case class Query(game: String)
+    case class Query(game: Long)
     case class Update()
-    def props(out: ActorRef, gameDAO: GameDAO) =
-        Props(classOf[SteamActor], out, gameDAO)
+    def props(gameDAO: GameDAO, offerDAO: OfferDAO) =
+        Props(classOf[SteamActor], gameDAO, offerDAO)
 }
 
-class PsActor extends Actor{
+class PsActor(gameDAO: GameDAO, offerDAO: OfferDAO) extends Actor{
     import context._
     import actors.PsActor._
-
     // https://store.playstation.com/store/api/chihiro/00_09_000/tumbler/CL/es/999/<query>?suggested_size=5&mode=game
+    /*
+        ...
+        links = [
+            {
+                default_sku = {
+                    display_price = "US$22.99"
+                    price = 2299
+                    rewards = [
+                        {
+                            discount = 50
+                            price = 1149
+                            display_price = "US$11.49"
+                            isPlus = False
+                            start_date = "2016-06-07T15:00:00Z"
+                            end_date = "2016-06-13T15:00:00Z"
+                        }
+                    ]
+                    ...
+                }
+                metadata = {
+                    genre, game_genre, game_subgenre, playable_platform, ...
+                }
+                name = "Resident Evil 4"
+                playable_platform = [ PS3 ]
+                release_date = "2011-09-20T00:00:00Z"
+                url = "bla"
+        ]
+
+     */
+    case class OfferData(id: String, platform: String, url: String, base_price: Double, discounted_price: Double, start_date: String, end_date: String)
+    implicit val offerRead: Reads[OfferData] = (
+        (__ \ "id").read[String] and
+        (__ \ "playable_platform")(0).read[String] and
+        (__ \ "url").read[String] and
+        (__ \ "default_sku" \ "price").read[Double] and
+        ((__ \ "default_sku" \ "rewards")(0) \ "price").read[Double] and
+        ((__ \ "default_sku" \ "rewards")(0) \ "start_date").read[String] and
+        ((__ \ "default_sku" \ "rewards")(0) \ "end_date").read[String])(OfferData.apply _)
+
+    implicit def  strToTimestamp(s: String): Timestamp = {
+        // start_date = "2016-06-07T15:00:00Z"
+        val date = (s split "T")(0)
+        val split_date = date split "-"
+        new Timestamp(new Date(split_date(0).toInt, split_date(1).toInt, split_date(2).toInt).getTime)
+    }
 
     def receive = {
         case Query(game) => {
-            // Obtener precios para $game
+            ???
         }
         case Update() => {
-            // Actualizar precios de la DB
+            /*val response: JsValue = ws.url(url).get().map {
+                resp => resp.json
+            }*/
+            val response: JsObject = ???
+            val game_ids: Map[String, Long] = ???
+
+            val new_offers = (response \ "links").as[List[OfferData]]
+            val valid_offers =
+                for {
+                    offer <- new_offers
+                    if game_ids contains offer.id
+                } yield Offer(0,
+                    offer.url,
+                    game_ids.get(offer.id) match {
+                        case Some(g) => g
+                        case None => 0
+                    },
+                    offer.platform match {
+                        case "PS3" => 1
+                        case _ => 2         // "PS4 TM"
+                    },
+                    "PlayStation Store",
+                    offer.start_date,
+                    offer.end_date,
+                    offer.base_price/100.0,
+                    offer.discounted_price/100.0)
+            offerDAO.insert(valid_offers)
         }
     }
+
 
 }
