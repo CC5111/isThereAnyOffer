fe1063081be8579b87c797bd0b1d9490194bf95c
Pedro Bustamante <ppbb15@gmail.com>
2016-08-10 16:33:17 -0400

    Corrección estilo Home
    Se modificó estilo de panel de filtros
    Se modificó estilo de títulos de filtro

--- public/javascripts/main_scripts.js
+++ public/javascripts/main_scripts.js
@@ -6,5 +6,14 @@ function trLink(url){
     );
 }
 
+$(document).ready(function() {
+    $('.panel-collapse').on('show.bs.collapse', function () {
+        console.log("Abierto");
+        $(this).siblings('.panel-heading').addClass('active');
+    });
 
-
+    $('.panel-collapse').on('hide.bs.collapse', function () {
+        console.log("Cerrado");
+        $(this).siblings('.panel-heading').removeClass('active');
+    });
+});
